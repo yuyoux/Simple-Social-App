6866ffbbc864c293108a15c95d13829ef804f473
Isaac <yuyoux95@163.com>
2020-02-13 13:50:12 +1100

    remove redux devtools

--- src/index.js
+++ src/index.js
@@ -8,15 +8,14 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import { compose } from "redux";
+// import { compose } from "redux";
 
-// enhencer
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  // compose(
+  applyMiddleware(thunk)
+  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
 );
 
 ReactDOM.render(
